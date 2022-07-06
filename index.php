59ab9611803c81633d28d5a1412a08f8da91a009
AMEN <amen@ymwl.edu.kg>
2022-07-06 19:50:19 +0800

    complete to 0.4

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
  * @博客：https://blog.ymypay.cn/
  * 湮灭网络工作室
  */
-const __webSite__ = __DIR__ . '\\'; //网站根目录,引用以此变量开始
+const __webSite__ = __DIR__ . '/'; //网站根目录,引用以此变量开始
 
 require_once __webSite__.'app.php';
